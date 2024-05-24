dc37efcb76da37e6cb4d8cea03e90daa3a3979fa
GergertSB <gergertsb@yandex.ru>
2024-05-24 18:59:00 +0700

    Update Homework7.py

--- Homework7.py
+++ Homework7.py
@@ -1,13 +1,8 @@
 # "Цикл for. Элементы списка. Полезные функции в цикле"
 
 mobile = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
-for i in mobile:
-    print(' Я езжу на автомобиле марки', [i])
-
-# шаг два
-
-mobile = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
 cars_count = 0
 for i in range(len(mobile)):
+    print(' Я езжу на автомобиле марки', [i])
     cars_count += 10
     print(cars_count)
