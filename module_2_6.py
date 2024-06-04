e7267f33777b5647e1b149e56781a12cccba3377
GergertSB <gergertsb@yandex.ru>
2024-06-04 21:41:01 +0700

    Update module_2_6.py
    Применил метод .lower()

--- module_2_6.py
+++ module_2_6.py
@@ -3,7 +3,7 @@
 def single_root_words(root_word, *other_words):
     same_words = [] # создаем пустой список для слов
     for word in other_words: # проверяем, содержится ли слово в root_words
-        if root_word in word or word in root_word:
+        if root_word.lower() in word.lower() or word.lower() in root_word.lower():
             same_words.append(word)
     return same_words
 
