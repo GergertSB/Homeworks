6c5cc35dc774e801c8391c378a5f4f441477fda8
GergertSB <gergertsb@yandex.ru>
2024-07-11 22:38:52 +0700

    Add files via upload

---
+++ module_6_3.py
@@ -0,0 +1,51 @@
+# # обращаемся в родительский класс и вызываем его конструктор.
+# Задача "Мифическое наследование":
+"""
+Необходимо написать 3 класса:
+Horse - класс описывающий лошадь.
+Eagle - класс описывающий орла.
+Pegasus - класс описывающий пегаса.
+"""
+
+class Horse: # класс описывающий лошадь.
+    x_distance = 0 # атрибут класса, Пройденный путь
+    sound = 'Frrr' # атрибут класса, Звук, который издает лошадь
+
+    def __init__(self): # инициализатор, непонятно чего
+        self.sound = super().sound
+
+    def run(self, dx): # метод, Изменение дистанции, увеличивает x_distance на dx
+        self.x_distance += dx
+
+class Eagle: # класс описывающий орла.
+    y_distance = 0 # атрибут класса, Высота полёта
+    sound = 'I train, eat, sleep, and repeat' # атрибут класса, Звук, который издает орёл
+
+    def fly(self, dy): # метод, Изменение дистанции, увеличивает y_distance на dy
+        self.y_distance += dy
+
+class Pegasus(Horse, Eagle):
+
+    def move(self, dx, dy):
+        super().run(dx)
+        super().fly(dy)
+
+    def get_pos(self):
+        return self.x_distance, self.y_distance
+
+
+    def voice(self):
+        print(self.sound)
+
+
+p1 = Pegasus()
+
+print(p1.get_pos())
+p1.move(10, 15)
+print(p1.get_pos())
+p1.move(-5, 20)
+print(p1.get_pos())
+
+p1.voice()
+
+
