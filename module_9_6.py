22d099fb15d7276bfa73c6adeed16f7c89943a54
GergertSB <gergertsb@yandex.ru>
2024-08-15 20:31:31 +0700

    Update module_9_6.py

--- module_9_6.py
+++ module_9_6.py
@@ -14,19 +14,11 @@
 '''
 
 def all_variants(text):
-
-    for f in range(len(text)):
-        n = text[f]
-        yield n
-    for k in range(len(text)-1):
-        m = text[k]
-        p = text[k+1]
-        yield m + p
-    for d in range(len(text)):
-        h = text[d]
-        if h not in m+p:
-            yield h + m + p
-
+    x = 0
+    y = 0
+    for x in range(len(text)):
+        for y in range(x + 1, len(text) + 1):
+            yield text[x:y]
 
 a = all_variants("abc")
 for i in a:
